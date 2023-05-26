de791e6a1c1d9e00c12288967656579debfe7231
hsb <hsb@khu.ac.kr>
2023-05-26 22:07:30 +0900

    Add files via upload

---
+++ lib/image_utils.py
@@ -0,0 +1,93 @@
+import cv2
+import numpy as np
+import torch
+from torch.autograd import Variable
+
+
+def preprocess_image(img, cuda=False):
+    means=[0.5056, 0.5056, 0.5056]
+    stds=[0.252, 0.252, 0.252]
+
+    preprocessed_img = img.copy()[: , :, ::-1]#change BGR to RGB
+    for i in range(3):
+        preprocessed_img[:, :, i] = preprocessed_img[:, :, i] - means[i]
+        preprocessed_img[:, :, i] = preprocessed_img[:, :, i] / stds[i]
+    preprocessed_img = \
+        np.ascontiguousarray(np.transpose(preprocessed_img, (2, 0, 1)))#change H,W,C -> C,H,W
+    preprocessed_img = torch.from_numpy(preprocessed_img)#img to Tensor
+    preprocessed_img.unsqueeze_(0)#add extra dimension for batch
+    if cuda:
+        preprocessed_img = Variable(preprocessed_img.cuda(), requires_grad=True)
+    else:
+        preprocessed_img = Variable(preprocessed_img, requires_grad=True)
+
+    return preprocessed_img
+
+
+def save_as_gray_image(img, filename, percentile=99,colormap=cv2.COLORMAP_OCEAN):
+    img_2d = np.sum(img, axis=0)#multi channel ->single channel
+    span = abs(np.percentile(img_2d, percentile))
+    #span = np.percentile(img_2d, percentile)
+    vmin = -span
+    vmax = span
+    img_2d = np.clip((img_2d - vmin) / (vmax - vmin), -1, 1)
+
+
+    # Apply color mapping
+    heatmap = cv2.applyColorMap(np.uint8(255 * img_2d), colormap)
+    heatmap = np.float32(heatmap) / 255
+
+    cv2.imwrite(filename, np.uint8(255 * heatmap))
+    #cv2.imwrite(filename, img_2d*255)    
+   
+
+    return
+
+def show_cam_on_image_SG(img: np.ndarray,
+                      mask: np.ndarray,
+                      use_rgb: bool = False,
+                      colormap: int = cv2.COLORMAP_JET,
+                      image_weight: float = 0.5) -> np.ndarray:
+    """ This function overlays the cam mask on the image as an heatmap.
+    By default the heatmap is in BGR format.
+
+    :param img: The base image in RGB or BGR format.
+    :param mask: The cam mask.
+    :param use_rgb: Whether to use an RGB or BGR heatmap, this should be set to True if 'img' is in RGB format.
+    :param colormap: The OpenCV colormap to be used.
+    :param image_weight: The final result is image_weight * img + (1-image_weight) * mask.
+    :returns: The default image with the cam overlay.
+    """
+    img_2d = np.sum(mask, axis=0)#multi channel ->single channel
+    #span = abs(np.percentile(img_2d, 90))
+    span = np.percentile(img_2d, 99)
+    vmin = -span
+    vmax = span
+    img_2d = np.clip((img_2d - vmin) / (vmax - vmin), 0, 1)   
+   
+    heatmap = cv2.applyColorMap(np.uint8(255 * img_2d), colormap)
+    if use_rgb:
+        heatmap = cv2.cvtColor(heatmap, cv2.COLOR_BGR2RGB)
+    heatmap = np.float32(heatmap) / 255
+
+    if np.max(img) > 1:
+        raise Exception(
+            "The input image should np.float32 in the range [0, 1]")
+
+    if image_weight < 0 or image_weight > 1:
+        raise Exception(
+            f"image_weight should be in the range [0, 1].\
+                Got: {image_weight}")
+
+    cam = (1 - image_weight) * heatmap + image_weight * img
+    cam = cam / np.max(cam)
+    return np.uint8(255 * cam)
+
+
+
+def save_cam_image(img, mask, filename):
+    heatmap = cv2.applyColorMap(np.uint8(255*mask), cv2.COLORMAP_JET)
+    heatmap = np.float32(heatmap) / 255
+    cam = heatmap + np.float32(img)
+    cam = cam / np.max(cam)
+    cv2.imwrite(filename, np.uint8(255 * cam))
